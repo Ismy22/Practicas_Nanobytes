bba121d514a808180b3b43053fa1d5aa93264c5b
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-08 11:41:29 +0100

    gap_archivar_pedidos/

--- gap_archivar_pedidos/models/invoicing_archive.py
+++ gap_archivar_pedidos/models/invoicing_archive.py
@@ -3,4 +3,4 @@ from odoo import models, fields, api
 class AccountMove(models.Model):
     _inherit = "sale.order"
 
-    active = fields.Boolean(string="active", default=True)
+    active = fields.Boolean(string="Active", default=True)
