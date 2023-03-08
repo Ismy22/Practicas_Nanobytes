dad22a8e4f596c574ed250df53aa223e39ec3143
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-08 14:44:12 +0100

    primer intento archivar lineas de pedido

--- gap_archivar_pedidos/models/invoicing_archive.py
+++ gap_archivar_pedidos/models/invoicing_archive.py
@@ -1,13 +1,13 @@
 from odoo import models, fields, api
 
-class AccountMove(models.Model):
+class Saleorder(models.Model):
     _inherit = "sale.order"
 
     active = fields.Boolean(string="Active", default=True)
 
     @api.multi
     def action_archive(self):
-        res = super(AccountMove, self).action_archive()
+        res = super(Saleorder, self).action_archive()
 
         for line in self.order_line:
             line.action_cancel()
