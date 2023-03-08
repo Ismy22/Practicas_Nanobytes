fd766211d60a5233f09678ea101920ed7f166a08
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-08 14:39:44 +0100

    primer intento archivar lineas de pedido

--- gap_archivar_pedidos/models/invoicing_archive.py
+++ gap_archivar_pedidos/models/invoicing_archive.py
@@ -4,3 +4,14 @@ class AccountMove(models.Model):
     _inherit = "sale.order"
 
     active = fields.Boolean(string="Active", default=True)
+
+    @api.multi
+    def action_archive(self):
+        # Llama a la función de archivar del modelo padre
+        res = super(SaleOrder, self).action_archive()
+
+        # Itera sobre todas las líneas de pedido y las archiva también
+        for line in self.order_line:
+            line.action_cancel()
+
+        return res
