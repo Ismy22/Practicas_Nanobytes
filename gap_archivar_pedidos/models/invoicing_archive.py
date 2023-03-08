74bad18a1cf2b5f2fb46aa5d0f3c23a29d7032c1
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-08 10:45:53 +0100

    gap_archivar_pedidos/

--- gap_archivar_pedidos/models/invoicing_archive.py
+++ gap_archivar_pedidos/models/invoicing_archive.py
@@ -3,9 +3,4 @@ from odoo import models, fields, api
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    @api.model
-    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
-        if view_type == 'form':
-            view_ref = 'mymodule.view_move_form_custom'
-            view_id = self.env['ir.ui.view'].search([('name', '=', view_ref)]).id or view_id
-        return super(AccountMove, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
+    active = fields.Boolean(string="active", default=True)
