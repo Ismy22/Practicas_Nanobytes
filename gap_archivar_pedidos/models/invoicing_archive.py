47300b4ccbb096c1b3a5f4b249e9aec0523f3bf6
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-09 13:35:50 +0100

    intento cambiar a campo relacionado el active

--- gap_archivar_pedidos/models/invoicing_archive.py
+++ gap_archivar_pedidos/models/invoicing_archive.py
@@ -7,22 +7,22 @@ class Saleorder(models.Model):
 
     active = fields.Boolean(string="Active", default=True)
 
-    def action_archive(self):
-        res = super(Saleorder, self).action_archive()
-        for line in self.order_line:
-            line.write({'active': False})
-        return res
+    # def action_archive(self):
+    #     res = super(Saleorder, self).action_archive()
+    #     for line in self.order_line:
+    #         line.write({'active': False})
+    #     return res
     
-    def action_unarchive(self):
-        res = super(Saleorder, self).action_unarchive()
+    # def action_unarchive(self):
+    #     res = super(Saleorder, self).action_unarchive()
 
-        for id in self:
-            lineaPedido = self.env['sale.order.line'].search(domain=[('order_id', '=', id.id), ('active', '=', False)])
-            for line in lineaPedido:
-                line.write({'active': True})
-            return res
+    #     for id in self:
+    #         lineaPedido = self.env['sale.order.line'].search(domain=[('order_id', '=', id.id), ('active', '=', False)])
+    #         for line in lineaPedido:
+    #             line.write({'active': True})
+    #         return res
        
 class Saleorderline(models.Model):
     _inherit = "sale.order.line"
 
-    active = fields.Boolean(string="Active", default=True)
+    active = fields.Boolean(string="Active", related=Saleorder.active, default=True)
