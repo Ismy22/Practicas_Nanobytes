dc0c9f697a792354029e9dd7a01f58676b559f14
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-09 14:48:03 +0100

    creacion wizard intento 1

--- gap_archivar_pedidos/models/wizard_res_partner.py
+++ gap_archivar_pedidos/models/wizard_res_partner.py
@@ -4,5 +4,5 @@ class wizarResPartner(models.TransientModel):
     _name = 'res.partner.wizard'
     _description = 'Wizard for change user/proyect'
 
-    user = fields.one2many('res.partner', string='Usuario')
+    user = fields.One2many('res.partner', string='Usuario')
     project = fields.Many2one('proyect.proyect', string='Proyecto')
