9103ddfc7fae722f02f78b9e7eed9b2a673f9aee
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-10 13:56:44 +0100

    intento poner many2one

--- gap_archivar_pedidos/wizard/wizard_contactos.py
+++ gap_archivar_pedidos/wizard/wizard_contactos.py
@@ -4,10 +4,10 @@ class wizarResPartner(models.TransientModel):
     _name = 'res.user.wizard'
     _description = 'Wizard for change user/proyect'
 
-    # user = fields.Many2one('res.user', string='Usuario')
-    # project = fields.Many2one('proyect.proyect', string='Proyecto')
-    user = fields.Char(String='Usuario')
-    project = fields.Char(String='Proyecto')
+    user = fields.Many2one('res.user', string='Usuario')
+    project = fields.Many2one('proyect.proyect', string='Proyecto')
+    # user = fields.Char(String='Usuario')
+    # project = fields.Char(String='Proyecto')
 
     def save_contact_wizard(self):
         return
