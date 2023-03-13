318b26fb0ff43abddb787907b4995e623ea06ffe
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-13 12:24:00 +0100

    funciona con security, push justo antes de empezar con listar

--- gap_archivar_pedidos/wizard/wizard_contactos.py
+++ gap_archivar_pedidos/wizard/wizard_contactos.py
@@ -9,7 +9,7 @@ class wizarResPartner(models.TransientModel):
     _description = 'Wizard for change user/proyect'
 
     user = fields.Many2one('res.users', string='Usuario')
-    project = fields.Many2one('project.project', string='Proyecto', domain="[('user_id','=', 'self.user_id')]")
+    project = fields.Many2one('project.project', string='Proyecto', default=lambda self: self._context.get('default_partner_id'))
 
     def save_contact_wizard(self):
         if not self.user or not self.project:
