54d0f6a8c79b1b291cd2ed7a787dbba52d84a04e
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-13 11:36:54 +0100

    funciona con security, push justo antes de empezar con listar

--- gap_archivar_pedidos/wizard/wizard_contactos.py
+++ gap_archivar_pedidos/wizard/wizard_contactos.py
@@ -1,5 +1,7 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
+import logging
+logger = logging.getLogger(__name__)
 
 
 class wizarResPartner(models.TransientModel):
@@ -7,13 +9,16 @@ class wizarResPartner(models.TransientModel):
     _description = 'Wizard for change user/proyect'
 
     user = fields.Many2one('res.users', string='Usuario')
-    project = fields.Char(string='Proyecto', compute='get_user_projects')
+    project = fields.Many2one('project.project', string='Proyecto', compute='get_user_projects')
 
     def save_contact_wizard(self):
         if not self.user or not self.project:
             raise ValidationError(_('Debe seleccionar un usuario y un proyecto.'))
 
         project_record = self.env['project.project'].search([('id', '=', self.project.id)])
+        logger.info('-------------------self.project----------')
+        logger.info(project_record)
+        logger.info('---------fin self.project-----------')
         if project_record:
             project_record  
         else:
@@ -26,6 +31,12 @@ class wizarResPartner(models.TransientModel):
     def get_user_projects(self):
         if self.user:
             projects = self.env['project.project'].search([('user_id', '=', self.user.id)])
+            logger.info('-------------------project----------')
+            logger.info(projects)
+            logger.info('---------fin project-----------')
             self.project = projects.ids
+            logger.info('-------------------self.project2----------')
+            logger.info(self.project)
+            logger.info('---------fin self.project2-----------')
         else:
             self.project = False
