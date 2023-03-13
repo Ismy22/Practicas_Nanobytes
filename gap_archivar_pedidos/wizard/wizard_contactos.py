d3a35d4c06ec3c1609b939cf165673211afe25dd
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-13 10:20:37 +0100

    funciona con security, push justo antes de empezar con listar

--- gap_archivar_pedidos/wizard/wizard_contactos.py
+++ gap_archivar_pedidos/wizard/wizard_contactos.py
@@ -1,6 +1,13 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
+class Project(models.Model):
+    _name = 'user.project'
+    _description = 'User Project'
+    
+    name = fields.Char(string='Name')
+    user_id = fields.Many2one('res.users', string='User')
+
 class wizarResPartner(models.TransientModel):
     _name = 'res.user.wizard'
     _description = 'Wizard for change user/proyect'
@@ -22,3 +29,10 @@ class wizarResPartner(models.TransientModel):
             raise ValidationError(_('Error al asignar el proyecto'))
 
         return {'type': 'ir.actions.act_window_close'}
+    
+    @api.onchange('user')
+    def _onchange_user(self):
+        if self.user:
+            self.project_ids = self.env['user.project'].search([('user_id', '=', self.user.id)])
+        else:
+            self.project_ids = False
