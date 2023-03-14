4b0552b73f69b5f8702ff32cfd6ac21e41c97749
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-14 10:00:45 +0100

    funciona, ahora falta listar proyectos, pero ya reasigna

--- gap_archivar_pedidos/wizard/wizard_contactos.py
+++ gap_archivar_pedidos/wizard/wizard_contactos.py
@@ -44,7 +44,7 @@ class wizarResPartner(models.TransientModel):
         logger.info(user_id)
         logger.info('------------------------FIN USER_ID------------------------------')
         res.update({
-            'user_id': contact_id,
+            'user_id': partner,
         })
         
         return res
