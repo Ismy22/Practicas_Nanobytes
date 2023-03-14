a29986007d976e1c4b8b6107c2c276b3373e832e
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-14 11:13:46 +0100

    funciona, ahora falta listar proyectos, pero ya reasigna

--- gap_archivar_pedidos/wizard/wizard_contactos.py
+++ gap_archivar_pedidos/wizard/wizard_contactos.py
@@ -30,10 +30,16 @@ class wizarResPartner(models.TransientModel):
     def default_get(self, fields):
         res = super(wizarResPartner, self).default_get(fields)
         contact_id = self._context.get('default_partner_id')
-        id_user_por_partner = self.env['res.users'].search([('partner_id', '=', contact_id)])
-        #proyecto_por_usurario = self.env['project.project'].search([('partner_id', '=', contact_id)])
+        user_por_partner = self.env['res.users'].search([('partner_id', '=', contact_id)])
+        id_usuario = user_por_partner.id
+        logger.info('------------id_usuario----------')
+        logger.info(id_usuario)
+        logger.info('------------FIN id_usuario----------')
+        proyecto_por_usurario = self.env['project.project'].search([('user_id', '=', id_usuario)])
 
-        res.update({'user_id': id_user_por_partner,})
+        res.update({'user_id': user_por_partner,})
+        res.update({'project_id': proyecto_por_usurario,})
+
         
         return res
 
