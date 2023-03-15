9ee5bdcaea751430b42a9a3dc3d416bb8f96499b
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-15 11:14:12 +0100

    primera subida itscooter

--- gap_itscooter/models/inherit_product.py
+++ gap_itscooter/models/inherit_product.py
@@ -1,4 +1,7 @@
 from odoo import models, fields, api
+import csv
+import base64
+import requests
 import logging
 logger = logging.getLogger(__name__)
 
@@ -11,3 +14,27 @@ class Products(models.Model):
     EAN = fields.Char(String='EAN')
     
 
+class ProductImport(models.TransientModel):
+    _name = 'product.import'
+
+    
+
+    @api.multi
+    def import_products(self):
+        url="https://www.gm2online.es/amfeed/feed/download?id=23&file=StockGm2.csv"
+        response = requests.get(url)
+        if response.ok:
+            reader = csv.DictReader(response.content.decode('utf-8').splitlines())
+            Product = self.env['product.product']
+            for row in reader:
+                vals = {
+                    'SKU': row['SKU'],
+                    'EAN': row['EAN'],
+                    'name': row['Name'],
+                    'Price_cost': row['Price'],
+                    'QTY': row['QTY']
+                }
+                product = Product.create(vals)
+            return {'type': 'ir.actions.act_window_close'}
+        else:
+            return {'type': 'ir.actions.act_window_close', 'warning': 'Unable to fetch data from URL.'}
