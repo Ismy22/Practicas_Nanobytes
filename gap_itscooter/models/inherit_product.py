0252c19d98a7df1fd305692ce29b706d218ba24c
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-15 12:44:53 +0100

    intentando que lea el csv...

--- gap_itscooter/models/inherit_product.py
+++ gap_itscooter/models/inherit_product.py
@@ -18,35 +18,35 @@ class Products(models.Model):
         product = super(Product, self).create(vals)
         return product
 
-    def create_products_from_csv():
-        file_path = 'C:/Users/ismae/Downloads/StockGm2.csv'
-        with open(file_path) as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                sku = row.get('SKU')
-                ean = row.get('EAN')
-                name = row.get('Name')
-                price = row.get('Price')
-                qty = row.get('Qty')
+def create_products_from_csv():
+    file_path = 'C:/Users/ismae/Downloads/StockGm2.csv'
+    with open(file_path) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            sku = row.get('SKU')
+            ean = row.get('EAN')
+            name = row.get('Name')
+            price = row.get('Price')
+            qty = row.get('Qty')
 
-                if not sku or not name or not price:
-                    # Ignorar filas sin SKU, nombre o precio
-                    continue
+            if not sku or not name or not price:
+                # Ignorar filas sin SKU, nombre o precio
+                continue
 
-                # Asegurarse de que Qty y EAN tengan valores predeterminados
-                qty = qty or 0.0
-                ean = ean or ''
+            # Asegurarse de que Qty y EAN tengan valores predeterminados
+            qty = qty or 0.0
+            ean = ean or ''
 
-                # Crear objeto de producto
-                product = Products.create({
-                    'sku': sku,
-                    'ean': ean,
-                    'name': name,
-                    'price': float(price),
-                    'qty': float(qty)
-                })
+            # Crear objeto de producto
+            product = Products.create({
+                'sku': sku,
+                'ean': ean,
+                'name': name,
+                'price': float(price),
+                'qty': float(qty)
+            })
 
-                print(f'Creado producto {product.name} con SKU {product.sku}')
+            print(f'Creado producto {product.name} con SKU {product.sku}')
         
 
     
