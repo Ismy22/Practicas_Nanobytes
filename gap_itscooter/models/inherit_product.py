746f34479f5070dcc1db92182215399bcdf5319e
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-15 12:37:55 +0100

    primera subida itscooter

--- gap_itscooter/models/inherit_product.py
+++ gap_itscooter/models/inherit_product.py
@@ -15,30 +15,38 @@ class Products(models.Model):
     EAN = fields.Char(String='EAN')
 
     def create(self, vals):
-        product = super(Products, self).create(vals)
+        product = super(Product, self).create(vals)
         return product
 
     def create_products_from_csv():
-        file_path = 'C:\Users\ismae\Downloads\StockGm2.csv'
+        file_path = '/ruta/al/archivo.csv'
         with open(file_path) as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                sku = row['SKU']
-                ean = row['EAN']
-                name = row['Name']
-                qty = row['QTY']
-                price = row['Price']                
+                sku = row.get('SKU')
+                ean = row.get('EAN')
+                name = row.get('Name')
+                price = row.get('Price')
+                qty = row.get('Qty')
+
+                if not sku or not name or not price:
+                    # Ignorar filas sin SKU, nombre o precio
+                    continue
+
+                # Asegurarse de que Qty y EAN tengan valores predeterminados
+                qty = qty or 0.0
+                ean = ean or ''
 
                 # Crear objeto de producto
                 product = Products.create({
                     'sku': sku,
                     'ean': ean,
                     'name': name,
-                    'qty': qty,
-                    'price': price                    
+                    'price': float(price),
+                    'qty': float(qty)
                 })
 
                 print(f'Creado producto {product.name} con SKU {product.sku}')
-    
+        
 
     
