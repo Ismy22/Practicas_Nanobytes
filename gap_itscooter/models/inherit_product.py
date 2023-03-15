4899903473c7a609af1f3b20086ce5d42edf523a
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-15 10:12:54 +0100

    primera subida itscooter

--- gap_itscooter/models/inherit_product.py
+++ gap_itscooter/models/inherit_product.py
@@ -6,7 +6,7 @@ class Products(models.Model):
     _inherit = "product.template"
 
     Qty = fields.Integer(string="Cantidad total")
-    Price = fields.Float(string="Price")
+    Price_cost = fields.Float(string="Price")
     SKU = fields.Char(String='SKU')
     EAN = fields.Char(String='EAN')
     
