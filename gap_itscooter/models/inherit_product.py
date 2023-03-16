834bd8b8b82659f4db92d532295ed9d24b90cd8b
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-16 09:07:59 +0100

    recoge el csv y lo mete en productos, ya con una url y no fichero

--- gap_itscooter/models/inherit_product.py
+++ gap_itscooter/models/inherit_product.py
@@ -3,6 +3,7 @@ from odoo import models, fields, api
 import csv
 import base64
 import requests
+from io import StringIO
 import logging
 logger = logging.getLogger(__name__)
 
@@ -16,31 +17,35 @@ class Products(models.Model):
 
 
     def create_products_from_csv(self):
-        file_path = "/mnt/extra-addons/gap_itscooter/docs/StockGm2.csv"
-        with open(file_path, 'r') as csvfile:
-            
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                SKU = row.get('SKU')
-                EAN = row.get('EAN')
-                name = row.get('Name')
-                Price_cost = row.get('Price')
-                Qty = row.get('Qty')
+        url = "https://www.gm2online.es/amfeed/feed/download?id=23&file=StockGm2.csv"
+        response = requests.get(url)
+        #si respuesta erronea
+        if response.status_code != 200:
+            raise ValueError("Failed to download CSV file from URL")
 
-                if not SKU or not name or not Price_cost:
-                    # Ignorar filas sin SKU, nombre o precio
-                    continue
+        file_contents = StringIO(response.content.decode("utf-8"))
+        reader = csv.DictReader(file_contents)
+        for row in reader:
+            SKU = row.get('SKU')
+            EAN = row.get('EAN')
+            name = row.get('Name')
+            Price_cost = row.get('Price')
+            Qty = row.get('Qty')
 
+            if not SKU or not name or not Price_cost:
+                # Ignorar filas sin SKU, nombre o precio
+                continue
 
-                # Crear producto
-                vals = {
-                    'SKU': SKU,
-                    'EAN': EAN,
-                    'name': name,
-                    'Price_cost': float(Price_cost),
-                    'Qty': float(Qty)
-                }
-                product = self.env['product.template'].create(vals)
+            # Crear producto
+            vals = {
+                'SKU': SKU,
+                'EAN': EAN,
+                'name': name,
+                'Price_cost': float(Price_cost),
+                'Qty': float(Qty)
+            }
+            product = self.env['product.template'].create(vals)
+
         
 
     
