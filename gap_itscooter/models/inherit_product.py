6c9bf798292e997640b8493de008072dfe53a867
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-17 11:23:30 +0100

    intentando modificar web

--- gap_itscooter/models/inherit_product.py
+++ gap_itscooter/models/inherit_product.py
@@ -108,8 +108,8 @@ class Products(models.Model):
         @http.route()
         def index(self):
             env = request.env
-            products = Products(env)
-
+            products = env['product.template']
+            return products.export_products_to_csv()
 
 
         # el 173 es el id del archivo a descargar, hayq ue conseguir que se cambie con el fichero
