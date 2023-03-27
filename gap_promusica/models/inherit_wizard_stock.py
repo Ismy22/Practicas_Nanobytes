d074308e239b32b697e64f8ad0a0eca8f6333cc6
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-27 15:32:11 +0200

    prueba subir promusica

--- gap_promusica/models/inherit_wizard_stock.py
+++ gap_promusica/models/inherit_wizard_stock.py
@@ -1,11 +1,4 @@
 from odoo import models, fields
        
-class ReturnPicking(models.Model):
-    _inherit = "stock.return.picking"
-
-    location_id = fields.Many2one('stock.location', 'Return Location')
-
-
-
 
 
