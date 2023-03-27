0040b50b759ccfc5a4538ec4b7da00fcaf8b5640
Ismy Martínez Blanco <ismy2203@gmail.com>
2023-03-27 10:16:07 +0200

    prueba subir promusica

---
+++ gap_promusica/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Gestión ITScooter',
+    'sequence': -1003,
+    'summary': '',
+    'description': """ Gap csv ITScooter""",
+    'depends': ['stock'],
+    'data': [
+        'views/inherit_product_form.xml',
+    ],
+    'auto_install': False,
+    'application': True
+}
---
+++ gap_promusica/models/inherit_product.py
@@ -0,0 +1,99 @@
+from odoo import models, fields, api
+
+import csv
+import base64
+
+import tempfile
+from odoo import http
+import base64
+import requests
+from odoo import http
+from odoo.addons.web.controllers.home import Home as WebHome
+from odoo.http import request
+from io import StringIO
+
+
+import logging
+logger = logging.getLogger(__name__)
+
+class Products(models.Model):
+    _inherit = "product.template"
+
+    Qty = fields.Float(string="Cantidad total", default=0.0)
+    Price_cost = fields.Float(string="Price", default=0.0)
+    SKU = fields.Char(String='SKU')
+    EAN = fields.Char(String='EAN')
+
+    @api.model
+    def create_products_from_csv(self):
+        url = "https://www.gm2online.es/amfeed/feed/download?id=23&file=StockGm2.csv"
+        response = requests.get(url)
+        #si respuesta erronea
+        if response.status_code != 200:
+            raise ValueError("Failed to download CSV file from URL")
+
+        file_contents = StringIO(response.content.decode("utf-8"))
+        reader = csv.DictReader(file_contents)
+        for row in reader:
+            SKU = row.get('SKU')
+            EAN = row.get('EAN')
+            name = row.get('Name')
+            Price_cost = row.get('Price')
+            Qty = row.get('Qty')
+
+            if not SKU or not name or not Price_cost:
+                # Ignorar filas sin SKU, nombre o precio
+                continue
+
+            # Crear producto
+            vals = {
+                'SKU': SKU,
+                'EAN': EAN,
+                'name': name,
+                'Price_cost': float(Price_cost),
+                'Qty': float(Qty)
+            }
+            product = self.env['product.template'].create(vals)
+
+    @api.model
+    def export_products_to_csv(self):
+        # Leer todos los productos
+        products = self.env['product.template'].search([])
+
+        # Crear archivo CSV y escribir encabezados de columna
+        csv_data = StringIO()
+        writer = csv.writer(csv_data)
+        writer.writerow(['SKU', 'EAN', 'Name', 'Price', 'Qty'])
+
+        # Escribir cada producto en una fila
+        for product in products:
+            writer.writerow([product.SKU, product.EAN, product.name, product.Price_cost, product.Qty])
+
+        # Codificar archivo CSV como base64
+        csv_base64 = base64.b64encode(csv_data.getvalue().encode('utf-8'))
+
+        # Crear un objeto ir.attachment para el archivo CSV
+        attachment = self.env['ir.attachment'].create({
+            'name': 'products.csv',
+            'datas': csv_base64,
+            'type': 'binary',
+        })
+        logger.info('---------attachment-------')
+        logger.info(attachment)
+        logger.info('---------fin attachment-------')
+
+        #Obtener la URL de descarga del archivo desde Odoo 
+        url = self.env['ir.config_parameter'].sudo().get_param('web.base.url') + '/web/content/%s' % (attachment.id) + '?download=true&filename=%s' % attachment.name
+        logger.info('---------URL-------')
+        logger.info(url)
+        logger.info('---------fin URL-------')
+        # # Verificar si url es None
+        # if url is None:
+        #     raise UserError("Error: 'web.base.url' no está configurado en el archivo de configuración de Odoo")
+
+        # # Devolver acción para descargar el archivo CSV
+        return {
+            'type': 'ir.actions.act_url',
+            'url': url,
+            'target': 'self',
+        }
